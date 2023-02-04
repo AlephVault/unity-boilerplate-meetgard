f2805e1037f25d8659493afda8b65bca6d3b78ed
Luis Masuelli <luis@datagran.io>
2023-02-04 15:37:11 -0500

    Updating readme and adding some scripts

---
+++ clone-all.bat
@@ -0,0 +1,11 @@
+call git clone --branch 0.0.2 git@github.com:AlephVault/unity-support.git  ..\unity-support
+call git clone --branch 0.0.1 git@github.com:AlephVault/unity-support-generic.git ..\unity-support-generic
+call git clone --branch 0.0.1 git@github.com:AlephVault/unity-boilerplates.git ..\unity-boilerplates
+call git clone --branch 0.0.1 git@github.com:AlephVault/unity-layout.git ..\unity-layout
+call git clone --branch 0.0.1 git@github.com:AlephVault/unity-menu-actions.git ..\unity-menu-actions
+call git clone --branch 0.0.2 git@github.com:AlephVault/unity-binary.git ..\unity-binary
+call git clone --branch 0.0.3 git@github.com:AlephVault/unity-meetgard.git ..\unity-meetgard
+call git clone --branch 0.0.3 git@github.com:AlephVault/unity-meetgard-auth.git ..\unity-meetgard-auth
+call git clone --branch 0.0.3 git@github.com:AlephVault/unity-meetgard-scopes.git ..\unity-meetgard-scopes
+call git clone --branch 0.0.2 git@github.com:AlephVault/unity-remotestorage.git ..\unity-remotestorage
+
---
+++ clone-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git clone --branch 0.0.2 git@github.com:AlephVault/unity-support.git  ../unity-support
+git clone --branch 0.0.1 git@github.com:AlephVault/unity-support-generic.git ../unity-support-generic
+git clone --branch 0.0.1 git@github.com:AlephVault/unity-boilerplates.git ../unity-boilerplates
+git clone --branch 0.0.1 git@github.com:AlephVault/unity-layout.git ../unity-layout
+git clone --branch 0.0.1 git@github.com:AlephVault/unity-menu-actions.git ../unity-menu-actions
+git clone --branch 0.0.2 git@github.com:AlephVault/unity-binary.git ../unity-binary
+git clone --branch 0.0.3 git@github.com:AlephVault/unity-meetgard.git ../unity-meetgard
+git clone --branch 0.0.3 git@github.com:AlephVault/unity-meetgard-auth.git ../unity-meetgard-auth
+git clone --branch 0.0.3 git@github.com:AlephVault/unity-meetgard-scopes.git ../unity-meetgard-scopes
+git clone --branch 0.0.2 git@github.com:AlephVault/unity-remotestorage.git ../unity-remotestorage
+
