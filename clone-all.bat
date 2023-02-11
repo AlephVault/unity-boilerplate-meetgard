49780f18438ffa558f1b125a7bb1a1c6b826a8e0
Luis Masuelli <luis@datagran.io>
2023-02-11 10:18:01 -0500

    Bumping the meetgard dependency to 0.0.4

--- clone-all.bat
+++ clone-all.bat
@@ -4,7 +4,7 @@ call git clone --branch 0.0.1 git@github.com:AlephVault/unity-boilerplates.git .
 call git clone --branch 0.0.1 git@github.com:AlephVault/unity-layout.git ..\unity-layout
 call git clone --branch 0.0.1 git@github.com:AlephVault/unity-menu-actions.git ..\unity-menu-actions
 call git clone --branch 0.0.2 git@github.com:AlephVault/unity-binary.git ..\unity-binary
-call git clone --branch 0.0.3 git@github.com:AlephVault/unity-meetgard.git ..\unity-meetgard
+call git clone --branch 0.0.4 git@github.com:AlephVault/unity-meetgard.git ..\unity-meetgard
 call git clone --branch 0.0.3 git@github.com:AlephVault/unity-meetgard-auth.git ..\unity-meetgard-auth
 call git clone --branch 0.0.3 git@github.com:AlephVault/unity-meetgard-scopes.git ..\unity-meetgard-scopes
 call git clone --branch 0.0.2 git@github.com:AlephVault/unity-remotestorage.git ..\unity-remotestorage
--- clone-all.sh
+++ clone-all.sh
@@ -5,7 +5,7 @@ git clone --branch 0.0.1 git@github.com:AlephVault/unity-boilerplates.git ../uni
 git clone --branch 0.0.1 git@github.com:AlephVault/unity-layout.git ../unity-layout
 git clone --branch 0.0.1 git@github.com:AlephVault/unity-menu-actions.git ../unity-menu-actions
 git clone --branch 0.0.2 git@github.com:AlephVault/unity-binary.git ../unity-binary
-git clone --branch 0.0.3 git@github.com:AlephVault/unity-meetgard.git ../unity-meetgard
+git clone --branch 0.0.4 git@github.com:AlephVault/unity-meetgard.git ../unity-meetgard
 git clone --branch 0.0.3 git@github.com:AlephVault/unity-meetgard-auth.git ../unity-meetgard-auth
 git clone --branch 0.0.3 git@github.com:AlephVault/unity-meetgard-scopes.git ../unity-meetgard-scopes
 git clone --branch 0.0.2 git@github.com:AlephVault/unity-remotestorage.git ../unity-remotestorage
